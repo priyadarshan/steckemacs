6c58932c3656bd171962abdc53619d7a52699167
steckerhalter <steckerhalter@postmomentum.ch>
2013-04-20 02:31:52 +0200

    use stable branch for el-get

--- .emacs.el
+++ .emacs.el
@@ -56,13 +56,13 @@
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (setq el-get-install-skip-emacswiki-recipes t)
+
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
 
 (setq el-get-sources
       '(
